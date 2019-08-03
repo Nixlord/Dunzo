2abd6a7d4af04043fbd6f086ec38e3c9086a58cd
300067934 <shibasis.patnaik@myntra.com>
2019-08-03 11:41:24 +0530

    Added google-services.json

--- app/src/main/java/com/nixlord/dunzo/MainActivity.kt
+++ app/src/main/java/com/nixlord/dunzo/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.nixlord.dunzo
 
 import android.os.Bundle
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer
 import com.phoenixoverlord.pravega.base.BaseActivity
 import com.phoenixoverlord.pravega.toast
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,5 +17,11 @@ class MainActivity : BaseActivity() {
             toast("Hello World")
         }
 
+//        val image = FirebaseVisionImage.fromBitmap();
+        val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
+//        val result = detector.processImage(image)
+//            .addOnSuccessListener {
+//
+//            }
     }
 }
