3875104298b96a0d6038b7a883173f2c050754e2
300067932 <saurabh.choudary@myntra.com>
2019-08-03 19:17:51 +0530

    Function for get lowest/highest index, separate element list and clean element list of the markers added.

--- app/src/main/java/com/nixlord/dunzo/util/DataFusion.java
+++ app/src/main/java/com/nixlord/dunzo/util/DataFusion.java
@@ -2,7 +2,11 @@ package com.nixlord.dunzo.util;
 
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.phoenixoverlord.pravega.extensions.LoggerKt;
+import me.xdrop.fuzzywuzzy.FuzzySearch;
+import me.xdrop.fuzzywuzzy.model.ExtractedResult;
+
 import java.util.*;
+
 public class DataFusion {
     public static void createProduct(FirebaseVisionText visionText) {
 //        food/bill10
@@ -27,20 +31,62 @@ public class DataFusion {
         String address = "";
         String phoneNo = "";
         String resultText = visionText.getText();
-        print(resultText);
+        //print(resultText);
 
-        /*for(FirebaseVisionText.TextBlock block: visionText.getTextBlocks()){
+        for(FirebaseVisionText.TextBlock block: visionText.getTextBlocks()){
+            String blockText = block.getText();
+            print(blockText);
             if(headerCount-->0) {
-                String blockText = block.getText();
+
                 List<FirebaseVisionText.Line> lineList = block.getLines();
                 name = lineList.get(0).getText();
                 address = lineList.get(1).getText() + " " + lineList.get(2).getText() + " " + lineList.get(3).getText();
                 phoneNo = lineList.get(4).getText().replaceAll("Ph No. ", "");
             }
         }
-        print("Name: "+name+"\n Address: "+address+"\nPhone No.: "+phoneNo);*/
+        print("Name: "+name+"\n Address: "+address+"\nPhone No.: "+phoneNo);
         //LoggerKt.logDebug("DataFusion", "");
 
+    }
+    public static int getLowestIndex(HashMap<String, Integer> map){
+        Integer lowest = Integer.MAX_VALUE;
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            if(entry.getValue()!=-1 && entry.getValue()<lowest){
+                lowest = entry.getValue();
+            }
+        }
+        return lowest;
+    }
+    public static int getHighestIndex(HashMap<String,Integer> map){
+        Integer highest = -1;
+        for(Map.Entry<String, Integer> entry: map.entrySet()){
+            if(entry.getValue()!=-1 &&entry.getValue()>highest){
+                highest = entry.getValue();
+            }
+        }
+        return highest;
+    }
+    public static ArrayList<String> extract(int startIndex, int endIndex, ArrayList<String> elementList){
+        ArrayList<String> updatedElementList = (ArrayList<String>) elementList.subList(startIndex, endIndex+1);
+        return updatedElementList;
+    }
+    public static void separateElementList(ArrayList<String> numberElementList, ArrayList<String> nameElementList){
+        int index = 0;
+        for(String s:numberElementList){
+            try{
+                Float.parseFloat(s);
+            }
+            catch (Exception e){
+                nameElementList.add(numberElementList.remove(index));
+            }
+            index++;
+        }
+    }
+    public static void cleanStringElementList(ArrayList<String> toClear, ArrayList<String> from){
+        for(String s:toClear){
+            ExtractedResult extractedResult = FuzzySearch.extractOne(s, from);
+            from.remove(extractedResult.getIndex());
+        }
     }
     public static void print(String msg){
         LoggerKt.logDebug("DataFusion", msg);
