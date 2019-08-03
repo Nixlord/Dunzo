6e16c065d7cd5583da1f0882175442c5e8098f0f
Diksha65 <agarwaldiksha6596@gmail.com>
2019-08-03 18:55:50 +0530

    Building App

--- app/src/main/java/com/nixlord/dunzo/ml/TextScanner.kt
+++ app/src/main/java/com/nixlord/dunzo/ml/TextScanner.kt
@@ -4,56 +4,20 @@ import android.Manifest
 import android.net.Uri
 import com.google.firebase.ml.vision.FirebaseVision
 import com.google.firebase.ml.vision.common.FirebaseVisionImage
-import com.google.firebase.ml.vision.text.FirebaseVisionText
 import com.nixlord.dunzo.util.DataFusion
 import com.phoenixoverlord.pravega.base.BaseActivity
 import com.phoenixoverlord.pravega.extensions.logDebug
-import me.xdrop.fuzzywuzzy.FuzzySearch
 import java.io.File
 
 object TextScanner {
     val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
 
-    fun parts(firebaseText : FirebaseVisionText) {
-        val elements = arrayListOf<String>()
-
-        firebaseText.textBlocks.forEach {
-            it.lines.forEach {
-                it.elements.forEach {
-                    elements.add(it.text)
-                }
+    fun scan(activity: BaseActivity, image: File) {
+        val firebaseImage = FirebaseVisionImage.fromFilePath(activity, Uri.fromFile(image))
+        detector.processImage(firebaseImage)
+            .addOnSuccessListener {
+                //logDebug(it.text)
+                DataFusion.createProduct(it)
             }
-        }
-
-        val closest = elements.reduce { largest, current ->
-            if ( FuzzySearch.ratio(current, "item") > FuzzySearch.ratio(largest, "item"))
-                current
-            else
-                largest
-        }
-
-        logDebug("Logging from", "Closest word to item is $closest")
-    }
-
-    fun scan(activity: BaseActivity) {
-        activity.apply {
-            withPermissions(
-                Manifest.permission.CAMERA
-            ).execute {
-                takePhoto("Select Bill")
-                    .addOnSuccessListener {
-                        it.forEach { image ->
-                            val firebaseImage = FirebaseVisionImage.fromFilePath(activity, Uri.fromFile(image))
-                            detector.processImage(firebaseImage)
-                                .addOnSuccessListener {
-                                    //logDebug(it.text)
-                                    DataFusion.createProduct(it)
-                                    parts(it)
-                                }
-
-                        }
-                    }
-            }
-        }
     }
 }
