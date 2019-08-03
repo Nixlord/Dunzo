ea7ece845fdda2f070a4454e6fa1e5c11d572fe4
300067932 <saurabh.choudary@myntra.com>
2019-08-04 04:50:13 +0530

    Model Files in model and JSONParser in util

--- app/src/main/java/com/nixlord/dunzo/util/JSONParser.java
+++ app/src/main/java/com/nixlord/dunzo/util/JSONParser.java
@@ -1,9 +1,10 @@
 package com.nixlord.dunzo.util;
 
 import com.google.gson.*;
+import com.nixlord.dunzo.model.RecognitionResult;
 
 public class JSONParser {
-    public static void parser(){
+    public static void parser() {
 //        GsonBuilder builder = new GsonBuilder();
 //        builder.setPrettyPrinting();
 //
@@ -14,8 +15,12 @@ public class JSONParser {
 
         String jsonString = "{\"name\":\"Mahesh Kumar\", \"age\":21,\"verified\":false,\"marks\": [100,90,85]}";
         //create tree from JSON
-        JsonElement rootNode = parser.parse(jsonString);
-        JsonObject details = rootNode.getAsJsonObject();
-        JsonElement element = details.getAsJsonArray("recognitionResults").getAsJsonArray();
+//        JsonElement rootNode = parser.parse(jsonString);
+//        JsonObject details = rootNode.getAsJsonObject();
+//        JsonElement element = details.getAsJsonArray("recognitionResults").getAsJsonArray();
+
+        Gson g = new Gson();
+        RecognitionResult result = g.fromJson(jsonString, RecognitionResult.class);
     }
 }
+
