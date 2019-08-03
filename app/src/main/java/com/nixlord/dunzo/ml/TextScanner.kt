e7d4f2ef3c0711c0328196777812f98321b2fca7
300067934 <shibasis.patnaik@myntra.com>
2019-08-03 19:13:25 +0530

    Created Closest Match Logic. Have to test.

--- app/src/main/java/com/nixlord/dunzo/ml/TextScanner.kt
+++ app/src/main/java/com/nixlord/dunzo/ml/TextScanner.kt
@@ -11,12 +11,21 @@ import com.phoenixoverlord.pravega.extensions.logDebug
 import me.xdrop.fuzzywuzzy.FuzzySearch
 import java.io.File
 
+const val THRESHOLD = 0.7
+
 object TextScanner {
+
     val detector = FirebaseVision.getInstance().onDeviceTextRecognizer
 
-    fun parts(firebaseText : FirebaseVisionText) {
-        val elements = arrayListOf<String>()
 
+    val firstMarkers = arrayListOf<String>("item", "name", "particulars", "price", "qty", "total", "amount")
+    val secondMarkers = arrayListOf<String>("grand", "total", "subtotal", "vat")
+
+    fun getElements(
+        firebaseText: FirebaseVisionText
+    ) : ArrayList<String> {
+
+        val elements = arrayListOf<String>()
         firebaseText.textBlocks.forEach {
             it.lines.forEach {
                 it.elements.forEach {
@@ -24,16 +33,62 @@ object TextScanner {
                 }
             }
         }
+        return elements
+    }
 
-        val closest = elements.reduce { largest, current ->
+    fun parts(firebaseText : FirebaseVisionText) {
+        val elements = getElements(firebaseText)
+
+        val mapFirstMarkers = hashMapOf<String, Int>()
+        val mapSecondMarkers = hashMapOf<String, Int>()
+
+        firstMarkers.forEach { mapFirstMarkers[it] = -1 }
+        secondMarkers.forEach { mapSecondMarkers[it] = -1 }
+
+        fun assignClosestMatch(
+            map: HashMap<String, Int>,
+            target : String,
+            currentWord: String,
+            currentIndex: Int
+        ) {
+            val match = FuzzySearch.ratio(target, currentWord)
+            if (match > THRESHOLD) {
+                map[target]?.let { index ->
+                    if (index == -1)
+                        map[target] = currentIndex
+                    else {
+                        //Exists already.
+                        val previousWord = elements[index]
+                        val oldMatch = FuzzySearch.ratio(previousWord, target)
+
+                        if (match > oldMatch)
+                            map[target] = currentIndex
+                    }
+                }
+            }
+        }
+
+        elements.mapIndexed { index, word ->
+            firstMarkers.forEach { target ->
+                assignClosestMatch(mapFirstMarkers, target, word, index)
+            }
+
+            secondMarkers.forEach { target ->
+                assignClosestMatch(mapSecondMarkers, target, word, index)
+            }
+        }
+
+        fun closest() = elements.reduce { largest, current ->
             if ( FuzzySearch.ratio(current, "item") > FuzzySearch.ratio(largest, "item"))
                 current
             else
                 largest
         }
 
-        logDebug("Logging from", "Closest word to item is $closest")
+        logDebug("Logging from", "Closest word to item is ${closest()}")
     }
+
+
 
     fun scan(activity: BaseActivity) {
         activity.apply {
